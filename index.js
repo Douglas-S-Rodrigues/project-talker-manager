a35d0990be79ad33478e603a1c1fa3e32c82da50
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-06-21 16:05:04 -0300

    feat: rota para talkers com id

--- index.js
+++ index.js
@@ -13,13 +13,21 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', async (_request, response) => { 
+app.get('/talker', (_request, response) => { 
   try {
     const talkers = fs.readFileSync('./talker.json', 'utf8');
     return response.status(200).json(JSON.parse(talkers));
   } catch (error) {
     return response.status(200).json([]);
   }
+});
+
+app.get('/talker/:id', (request, response) => {
+    const { id } = request.params;
+    const talkers = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
+    const searchId = talkers.find((talkerId) => talkerId.id === Number(id));
+    if (searchId) return response.status(200).json(searchId);
+    return response.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 app.listen(PORT, () => {
