1062deeeffcf6d5ffc8a7ab1d38bf4e3591ceebf
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-06-22 16:05:25 -0300

    fix: mudando middlewares para outro arquivo

---
+++ middlewares/emailValidate.js
@@ -0,0 +1,15 @@
+function middlewareValidateEmail(request, response, next) {
+  const { email } = request.body;
+  const reg = /\S+@\S+\.\S+/; // https://www.horadecodar.com.br/2020/09/07/expressao-regular-para-validar-e-mail-javascript-regex/
+  const validate = reg.test(email);
+
+  if (!email) {
+    return response.status(400).json({ message: 'O campo "email" é obrigatório' }); 
+  }    
+  if (!validate) {
+    return response.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  return next();
+}
+
+module.exports = middlewareValidateEmail;
---
+++ middlewares/passwordValidate.js
@@ -0,0 +1,12 @@
+function middlewareValidatePassword(request, response, next) {
+  const { password } = request.body;
+  if (!password) {
+    return response.status(400).json({ message: 'O campo "password" é obrigatório' });
+  } 
+  if (password.length < 6) {
+    return response.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  } 
+  return next();
+}
+
+module.exports = middlewareValidatePassword;
