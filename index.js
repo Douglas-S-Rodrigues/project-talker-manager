4d4fe5f341e94d6eec00f79f116c00b18f5ede43
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-06-22 22:50:00 -0300

    fix: colocando req 1 e 2 async

--- index.js
+++ index.js
@@ -22,18 +22,18 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', (_request, response) => { 
+app.get('/talker', async (_request, response) => { 
   try {
-    const talkers = fs.readFileSync('talker.json', 'utf8');
-    return response.status(200).json(JSON.parse(talkers));
+    const talkers = await fs.readFile('talker.json', 'utf8');
+    return response.status(200).json(await JSON.parse(talkers));
   } catch (error) {
     return response.status(200).json([]);
   }
 });
 
-app.get('/talker/:id', (request, response) => {
+app.get('/talker/:id', async (request, response) => {
     const id = Number(request.params.id);
-    const talkers = JSON.parse(fs.readFileSync('talker.json', 'utf8'));
+    const talkers = JSON.parse(await fs.readFile('talker.json', 'utf8'));
     const searchId = talkers.find((talkerId) => talkerId.id === id);
     if (searchId) return response.status(200).json(searchId);
     return response.status(404).json({ message: 'Pessoa palestrante não encontrada' });
@@ -52,15 +52,11 @@ middlewareValidateTalk,
 middlewareValidateWatchedAt,
 middlewareValidateRate,
 async (request, response) => {
-  try {
-    const { name, age, talk } = request.body;
-    const prevState = JSON.parse(await fs.readFile('talker.json', 'utf8'));
-    const newTalker = { id: prevState.length + 1, name, age, talk };
-    await fs.writeFile('talker.json', JSON.stringify([...prevState, newTalker]));
-    return response.status(201).json(newTalker);
-  } catch (error) {
-    console.error(error);
-  }
+  const { name, age, talk } = request.body;
+  const prevState = JSON.parse(await fs.readFile('talker.json', 'utf8'));
+  const newTalker = { id: prevState.length + 1, name, age, talk };
+  await fs.writeFile('talker.json', JSON.stringify([...prevState, newTalker]));
+  return response.status(201).json(newTalker);
 });
 
 app.listen(PORT, () => {
